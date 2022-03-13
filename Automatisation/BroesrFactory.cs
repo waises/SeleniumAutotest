cc3f12241a1f9b9ead59255f5ff30546660b1ecb
Waises <kkpavelp@gmail.com>
2022-03-14 00:43:39 +0300

    created class for BrowserFactory and data.json file for collecting data for tests

---
+++ Automatisation/BroesrFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Newtonsoft.Json;
+
+namespace Automatisation
+{
+    public class BroesrFactory
+    {
+        private static IWebDriver webDriver;
+
+
+        private static string baseUrl = "steam.com";
+        private static string browser = "";
+        public static IWebDriver WebDriverOptions;
+        
+        public static void Init()
+        {
+            switch (browser)
+            {
+                case "Chrome":
+                    webDriver = new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    break;
+                case "Firefox":
+                    webDriver = new FirefoxDriver();
+                    FirefoxOptions options = new FirefoxOptions();
+                    break;
+                default:
+                    webDriver= new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    break;
+
+            }
+            webDriver.Manage().Window.Maximize();
+            chromeOptions.AddArgument(@"--incognito");
+
+        }
+    }
+}
--- Automatisation/UnitTest1.cs
+++ Automatisation/UnitTest1.cs
@@ -1,18 +1,56 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace Automatisation
 {
     public class Tests
     {
+        IWebDriver driver ;
+        ChromeOptions options;
+
         [SetUp]
         public void Setup()
         {
+            options = new ChromeOptions();
+            options.AddArgument(@"--incognito");
+            
+            
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //options.AddArgument(@"--lang engl");
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://store.steampowered.com/");
+            Assert.IsTrue(driver.Title=="Добро пожаловать в Steam", $"{driver.Title}");
+
+
+            IWebElement about_bttn = driver.FindElement(By.XPath("//a[@class='menuitem' and contains(@href, 'about')]"));
+            string about_text = about_bttn.Text;
+
+
+            Assert.IsTrue(about_text == "О STEAM", $"problem with {about_bttn}");
+            about_bttn.Click();
+
+            IWebElement online_players = driver.FindElement(By.XPath("//div[@class= 'online_stat']//*"));
+            string online_text = online_players.Text;
+            Assert.IsTrue(online_text == "2141241",$"{online_text}");
+            
+
+
         }
+        [TearDown]
+        public void AfterTestSuit()
+        {
+            driver.Close();
+        }
+    }
+    class Singletom
+    {
+
     }
 }
